228360dcdaf699668608e1be4f4bb98ffd3a8b1b
DSMK2 <dropshipmk2@gmail.com>
2016-12-20 00:12:53 -0500

    updates

--- sql_test.js
+++ sql_test.js
@@ -13,7 +13,7 @@ app.post('/addColors', (req, res) => {
 	res.send('hurgh');
 });
 
-const server = app.listen(3000, function () {
+const server = app.listen(process.env.PORT || 3000, function () {
 	let host = server.address().address;
 	let port = server.address().port;
 	console.log(`Listening to http:\/\/${host}/${port}`); 
