cfae33d8d9a70ff06c8e574fd009803e04d3e92e
DSMK2 <dropshipmk2@gmail.com>
2016-12-21 01:24:27 -0500

    Updates, added .env file

--- sql_test.js
+++ sql_test.js
@@ -1,3 +1,5 @@
+// General
+require('dotenv').config();
 // App Handling
 const Express = require('express');
 const app = Express();
@@ -6,7 +8,25 @@ const MySQL = require('mysql2');
 
 
 app.get('/getColors', (req, res) => {
-	res.send('asdf');
+	let connection = MySQL.createConnection({
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		database: process.env.DB_NAME
+	});
+	
+	connection.execute('SELECT * FROM `color_fun`', (err, results, fields) {
+		if(err)
+			res.send(err);
+		
+		if(results)
+			res.send(results);
+			
+		if(fields)
+			res.send(fields);
+	});
+	
+	connection.end();
 });
 
 app.post('/addColors', (req, res) => {
